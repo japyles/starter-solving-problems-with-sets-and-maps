1651b800f2ac4d6bf97b3429a0e492e1b2533238
John Pyles <pylesjohn@gmail.com>
2021-10-18 01:34:52 -0400

    updated and passed all tests for intersection function

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,30 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+    if (b.length === 0) return [];
+
+    let sortA = a.sort();
+    let sortB = b.sort();
+
+    const every = sortA.every((val, index) => val === sortB[index]);
+    if (every) {
+        return a
+    }
+
+    let m = new Map();
+    let s = new Set();
+
+    const addAToMap = a.forEach((item, index) => {
+        m.set(index, item)
+      })
+  
+      const bInMap = b.map((item) => {
+        for (let value of m.values()) {
+          if (value === item) {
+            s.add(item)
+          }
+        }
+      })
+
+    return [...s]
+}
 
 module.exports = intersection;
